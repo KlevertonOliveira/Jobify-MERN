dd63eafc0a77e8eb62c41f3e61cf4beba2adde2b
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-10-22 12:33:14 -0300

    feat(server): add middleware and routers to server.ts
    Imports middleware and routers files and sets them up in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 
-import * as dotenv from 'dotenv';
-import express from 'express';
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import { errorHandler } from './middleware/errorHandler';
+import { notFound } from './middleware/notFound';
+import { authRouter } from './routes/authRoutes';
+import { jobsRouter } from './routes/jobsRoutes';
 
 dotenv.config();
 
@@ -12,11 +16,16 @@ const app = express();
 app.use(express.json());
 
 /* Routes */
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   return res.send('Hello, World!');
 });
 
-/* Error Handlers */
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
+/* Error handlers */
+app.use(notFound);
+app.use(errorHandler);
 
 /* Server Setup */
 const port = process.env.PORT || 5000;
@@ -24,8 +33,7 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log('Connected to DB');
-    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    app.listen(port, () => console.log(`Server is running on http://localhost:${port}...`));
 
   }
   catch (error) {
