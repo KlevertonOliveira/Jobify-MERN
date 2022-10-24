d33a3ebffb89510d012c66917f294e274e799b5c
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-10-24 10:01:48 -0300

    feat(server): add Register logic implementation

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,8 +1,21 @@
+import { User } from '@models/User';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from 'src/errors';
 
 export async function register(req: Request, res: Response) {
-  return res.status(StatusCodes.OK).json({
-    message: 'Register route'
-  })
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please, provide all values')
+  }
+
+  const emailAlreadyRegistered = await User.findOne({ email });
+
+  if (emailAlreadyRegistered) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create({ name, email, password });
+  return res.status(StatusCodes.OK).json({ user })
 }
