7f3fa016adb83a44aa5a9a37719f6415b32954bd
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-10-24 10:00:51 -0300

    feat(server): add errorHandler logic implementation

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import { getReasonPhrase, StatusCodes } from 'http-status-codes';
+import { Error } from 'mongoose';
+import { MongoServerError } from 'mongoose/node_modules/mongodb';
 
-export function errorHandler(err, req: Request, res: Response, next: NextFunction) {
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-    message: 'An error has ocurred'
+export function errorHandler(error, req: Request, res: Response, next: NextFunction) {
+  console.log(error);
+
+  const customError = {
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: error.message || getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+  }
+
+  if (error instanceof Error.ValidationError) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = (
+      `Please, provide: ${Object.values(error.errors).map(field => field.message).join(', ')}`
+    )
+  }
+
+  if (error instanceof MongoServerError) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = `${Object.keys(error.keyValue).join(', ')} already in use`
+  }
+
+  res.status(customError.statusCode).json({
+    message: customError.message
   })
 }
