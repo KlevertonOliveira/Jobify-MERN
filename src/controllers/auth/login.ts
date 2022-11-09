3f279a7c54b9acbc6e91ac47054be259d25ef742
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-09 16:14:07 -0300

    feat(server): implement login controller

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,8 +1,33 @@
+import { User } from '@models/User';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnauthenticatedError } from 'src/errors';
 
 export async function login(req: Request, res: Response) {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please, provide all values');
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid credentials');
+  }
+
+  const token = user.createToken();
+  user.password = undefined;
+
   return res.status(StatusCodes.OK).json({
-    message: 'Login route'
+    user,
+    token,
+    location: user.location
   })
 }
