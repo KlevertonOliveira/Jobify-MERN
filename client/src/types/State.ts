e9524a38523f4564387874cd861eb1a2d3f0a8a5
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-09 14:53:15 -0300

    refactor(client): update State type

--- client/src/types/State.ts
+++ client/src/types/State.ts
@@ -1,7 +1,12 @@
 import { Alert } from './Alert';
+import { User } from './User';
 
 export type State = {
   isLoading: boolean,
   showAlert: boolean,
   alert: Alert,
+  user: User | null,
+  token: string | null,
+  userLocation: string,
+  jobLocation: string
 }
