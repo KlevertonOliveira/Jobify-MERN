c654c3df5ae3a4b62336acbdc797a4c20b0a7dd4
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-10 12:08:56 -0300

    fix(client): change Landing button to redirecting Link
    Implements redirect functionality by replacing button tag with react-router-dom's Link to 'register'
    page.

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
@@ -15,9 +16,9 @@ export function Landing() {
             job <span>tracking</span> app
           </h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eius repudiandae aliquam quae molestias obcaecati maxime consequatur iusto, animi quas delectus suscipit eum necessitatibus dignissimos ex voluptatum, distinctio nihil repellendus.</p>
-          <button className='btn btn-hero'>
+          <Link to={'/register'} className='btn btn-hero'>
             Login/Register
-          </button>
+          </Link>
         </div>
         <img src={main} alt="job hunt" className='img main-img' />
       </div>
