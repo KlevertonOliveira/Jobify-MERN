bd02afd36fd639ee689089ae72237ff5db635316
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-17 14:50:38 -0300

    feat(server): implement updateUser controller
    Creates the implementation of updateUser controller and updates User model's pre save hook to
    prevent bug.

--- src/controllers/auth/updateUser.ts
+++ src/controllers/auth/updateUser.ts
@@ -1,8 +1,30 @@
+import { User } from '@models/User';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from 'src/errors';
 
 export async function updateUser(req: Request, res: Response) {
+  const { email, name, lastName, location } = req.body;
+
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError('Please, provide all values');
+  }
+
+  const { userId } = req.user;
+  const user = await User.findById(userId);
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createToken();
+
   return res.status(StatusCodes.OK).json({
-    message: 'Update user route'
+    user,
+    token,
+    location: user.location,
   })
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -56,9 +56,10 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethods>({
 });
 
 UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(this.password, salt);
-
   this.password = hashPassword;
 })
 
