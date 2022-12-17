0c193dc89222c3dbeab19f4a1ee3f083936d9eff
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-12-17 10:53:34 -0300

    refactor(client): rename reducer actions (following action.ts)

--- client/src/contexts/reducer.ts
+++ client/src/contexts/reducer.ts
@@ -1,89 +1,72 @@
 import { Alert } from '../types/Alert';
-import { State } from '../types/State';
+import { GlobalState } from '../types/GlobalState';
 import { User } from '../types/User';
 import {
+  API_OPERATION_BEGIN,
+  API_OPERATION_END,
+  AUTHENTICATE_USER,
   CLEAR_ALERT,
-  DISPLAY_ALERT, LOGOUT_USER, OPERATION_BEGIN, TOGGLE_SIDEBAR, USER_OPERATION_ERROR, USER_OPERATION_SUCCESS
+  DISPLAY_ALERT,
+  LOGOUT_USER,
+  TOGGLE_SIDEBAR
 } from './actions';
 import { initialState } from './appContext';
 
 type Action =
   | { type: typeof DISPLAY_ALERT, payload: { alert: Alert } }
   | { type: typeof CLEAR_ALERT }
-  | { type: typeof OPERATION_BEGIN }
   | { type: typeof TOGGLE_SIDEBAR }
-  | { type: typeof LOGOUT_USER }
+  | { type: typeof API_OPERATION_BEGIN }
+  | { type: typeof API_OPERATION_END }
   | {
-    type: typeof USER_OPERATION_SUCCESS, payload: {
+    type: typeof AUTHENTICATE_USER, payload: {
       user: User,
       token: string,
       location: string,
-      successAlertMessage: string,
     }
   }
-  | {
-    type: typeof USER_OPERATION_ERROR, payload: {
-      errorAlertMessage: string
-    }
-  }
+  | { type: typeof LOGOUT_USER }
 
-
-export function reducer(state: State, action: Action): State {
+export function reducer(state: GlobalState, action: Action): GlobalState {
   switch (action.type) {
     case DISPLAY_ALERT:
       return {
         ...state,
         showAlert: true,
-        alert: {
-          type: action.payload.alert.type,
-          message: action.payload.alert.message,
-        }
+        alert: action.payload.alert
       }
     case CLEAR_ALERT:
       return {
         ...state,
         showAlert: false,
       }
-    case OPERATION_BEGIN:
+    case TOGGLE_SIDEBAR:
+      return {
+        ...state,
+        showSidebar: !state.showSidebar
+      }
+    case API_OPERATION_BEGIN:
       return {
         ...state,
         isLoading: true,
       }
-    case USER_OPERATION_SUCCESS:
+    case API_OPERATION_END:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case AUTHENTICATE_USER:
       return {
         ...state,
         user: action.payload.user,
         token: action.payload.token,
         userLocation: action.payload.location,
-        jobLocation: action.payload.location,
-        isLoading: false,
-        showAlert: true,
-        alert: {
-          type: 'success',
-          message: action.payload.successAlertMessage,
-        }
-      }
-    case USER_OPERATION_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        showAlert: true,
-        alert: {
-          type: 'error',
-          message: action.payload.errorAlertMessage
-        }
-      }
-    case TOGGLE_SIDEBAR:
-      return {
-        ...state,
-        showSidebar: !state.showSidebar
       }
     case LOGOUT_USER:
       return {
         ...initialState,
         user: null,
         token: null,
-        jobLocation: '',
         userLocation: '',
       }
     default:
