d7561355500d8dd9f1a11bb0ab4f7ba57a75be96
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 17:37:41 -0300

    feat(server): implement updateJob controller

--- src/controllers/jobs/updateJob.ts
+++ src/controllers/jobs/updateJob.ts
@@ -1,8 +1,31 @@
+import Job from '@models/Job';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, NotFoundError } from 'src/errors';
+import { checkPermissions } from 'src/utils/checkPermissions';
 
 export async function updateJob(req: Request, res: Response) {
-  return res.status(StatusCodes.CREATED).json({
-    message: 'update job route'
-  })
+  const {
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (!company || !position) {
+    throw new BadRequestError('Please, provide all values');
+  }
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No jobs found with id ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  return res.status(StatusCodes.OK).json({ updatedJob });
 }
