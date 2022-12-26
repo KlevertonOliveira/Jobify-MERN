0b6d470894cec88dc301ffbf0bfaf6b7933d128f
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 17:36:15 -0300

    feat(server): implements UnauthorizedError
    Creates the implementation for UnauthorizedError class and imports it in index.ts (in errors
    folder).

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,9 +1,11 @@
 import { BadRequestError } from './BadRequestError';
 import { NotFoundError } from './NotFoundError';
 import { UnauthenticatedError } from './UnauthenticatedError';
+import { UnauthorizedError } from './UnauthorizedError';
 
 export {
   NotFoundError,
   BadRequestError,
-  UnauthenticatedError
+  UnauthenticatedError,
+  UnauthorizedError,
 };
