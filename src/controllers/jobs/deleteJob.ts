8578ebaba9384e49ca3b5f063d05704d8e87dc46
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 17:38:09 -0300

    feat(server): implement deleteJob controller

--- src/controllers/jobs/deleteJob.ts
+++ src/controllers/jobs/deleteJob.ts
@@ -1,8 +1,23 @@
+import Job from '@models/Job';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { NotFoundError } from 'src/errors';
+import { checkPermissions } from 'src/utils/checkPermissions';
 
 export async function deleteJob(req: Request, res: Response) {
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No jobs found with id ${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.remove();
+
   return res.status(StatusCodes.OK).json({
-    message: 'delete job route'
-  })
+    message: 'Success! Job removed',
+  });
 }
