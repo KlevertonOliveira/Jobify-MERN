faace357c4a74ef8e4bac91d5a3fd07efa7aa28b
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 15:47:06 -0300

    refactor(client): remove unused states from GlobalState
    remove isEditing and editingJobId from GlobalState.

--- client/src/types/GlobalState.ts
+++ client/src/types/GlobalState.ts
@@ -4,10 +4,8 @@ import { User } from './User';
 
 export interface GlobalState {
   isLoading: boolean;
-  isEditing: boolean;
   showAlert: boolean;
   showSidebar: boolean;
-  editingJobId: string;
   alert: Alert;
   user: User | null;
   token: string | null;
