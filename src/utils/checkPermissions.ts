ba0d252288ed20462b9d5247153e69633b24038f
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 17:37:21 -0300

    feat(server): implement checkPermission functionality
    Creates the implementation for checkPermissions functionality in the utils folder.

---
+++ src/utils/checkPermissions.ts
@@ -0,0 +1,10 @@
+import { Request } from 'express';
+import { UnauthorizedError } from 'src/errors';
+
+export function checkPermissions(
+  requestUser: Request['user'],
+  resourceUserId: Object
+) {
+  if (requestUser.userId === resourceUserId.toString()) return;
+  throw new UnauthorizedError('Not authorized to access this route');
+}
