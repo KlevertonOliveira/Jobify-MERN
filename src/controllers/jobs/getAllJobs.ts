090a980c344ab134a067d8df7aab1108fde00360
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-26 10:03:58 -0300

    feat(server): add implementation of getAllJobs controller

--- src/controllers/jobs/getAllJobs.ts
+++ src/controllers/jobs/getAllJobs.ts
@@ -1,8 +1,13 @@
+import Job from '@models/Job';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 export async function getAllJobs(req: Request, res: Response) {
-  return res.status(StatusCodes.CREATED).json({
-    message: 'get all jobs route'
+  const jobs = await Job.find({ createdBy: req.user.userId });  
+  
+  return res.status(StatusCodes.OK).json({ 
+    totalJobs: jobs.length,
+    jobs,
+    numberOfPages: 1,
   })
 }
