1da09fd0bcc30ae3c47d2cbaf14621ee196b5bc9
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2022-12-29 15:08:51 -0300

    feat(server): implement showStats controller

--- src/controllers/jobs/showStats.ts
+++ src/controllers/jobs/showStats.ts
@@ -1,8 +1,37 @@
+import Job from '@models/Job';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import mongoose from 'mongoose';
+
+type Status = {
+  pending?: number;
+  interview?: number;
+  declined?: number;
+};
 
 export async function showStats(req: Request, res: Response) {
-  return res.status(StatusCodes.CREATED).json({
-    message: 'show stats route'
-  })
+  const stats = await Job.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId as string),
+      },
+    },
+    { $group: { _id: '$status', total: { $sum: 1 } } },
+  ]);
+
+  const reducedStats: Status = stats.reduce((acc, status) => {
+    return { ...acc, [status._id]: status.total };
+  }, {});
+
+  const completeStats = {
+    pending: reducedStats.pending || 0,
+    interview: reducedStats.interview || 0,
+    declined: reducedStats.declined || 0,
+  };
+
+  let monthlyApplications = [];
+
+  return res
+    .status(StatusCodes.CREATED)
+    .json({ completeStats, monthlyApplications });
 }
