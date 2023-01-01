dedb8560430ee37b96c05e3e4c1c900b6359a629
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-01 18:26:30 -0300

    feat(client): implements SearchContainer component
    Creates component structure and implements its logic.

--- client/src/components/SearchContainer.tsx
+++ client/src/components/SearchContainer.tsx
@@ -1,3 +1,88 @@
-export default function SearchContainer() {
-  return <h2>Search Container</h2>
+import { ChangeEvent, FormEvent, useState } from 'react';
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { useAppContext } from '../contexts/appContext';
+import { JobSortOptions, JobStatusOptions, JobTypeOptions } from '../types/Job';
+import {
+  jobStatus,
+  jobTypes,
+  SearchFormValues,
+} from '../types/SearchFormValues';
+import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
+
+interface SearchContainerProps {
+  searchFormValues: SearchFormValues;
+  onChangeSearchFormValues: React.Dispatch<
+    React.SetStateAction<SearchFormValues>
+  >;
+  resetSearchFormFilters: () => void;
+}
+
+export default function SearchContainer({
+  searchFormValues,
+  onChangeSearchFormValues,
+  resetSearchFormFilters,
+}: SearchContainerProps) {
+  const {
+    state: { isLoading },
+  } = useAppContext();
+
+  function handleChange(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    if (isLoading) return;
+
+    onChangeSearchFormValues({
+      ...searchFormValues,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function clearFilters(e: FormEvent) {
+    e.preventDefault();
+    resetSearchFormFilters();
+  }
+
+  return (
+    <Wrapper>
+      <form className='form'>
+        <h4>Search Form</h4>
+        {/* search */}
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='search'
+            value={searchFormValues.search}
+            handleChange={handleChange}
+          />
+          {/* type */}
+          <FormRowSelect
+            name='type'
+            value={searchFormValues.type}
+            handleChange={handleChange}
+            optionsList={Object.values(jobTypes)}
+          />
+          {/* status */}
+          <FormRowSelect
+            name='status'
+            value={searchFormValues.status}
+            handleChange={handleChange}
+            optionsList={Object.values(jobStatus)}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name='sort'
+            value={searchFormValues.sort}
+            handleChange={handleChange}
+            optionsList={Object.values(JobSortOptions)}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={clearFilters}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
