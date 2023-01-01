7a947fcae4c2025f20802e04428fa0bd38d521c9
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-01 18:28:03 -0300

    refactor(client): refactor JobsContainer component
    Refactor JobsContainer component based on the changes made on getJobs function.

--- client/src/components/JobsContainer.tsx
+++ client/src/components/JobsContainer.tsx
@@ -1,24 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Wrapper from '../assets/wrappers/JobsContainer';
 import { useAppContext } from '../contexts/appContext';
+import { JobsData } from '../types/Job';
+import { SearchFormValues } from '../types/SearchFormValues';
 import Job from './Job';
 import Loading from './Loading';
+interface JobsContainerProps {
+  searchFormValues: SearchFormValues;
+}
 
-export default function JobsContainer() {
+const initialState: JobsData = {
+  jobs: [],
+  totalJobs: 0,
+};
+
+export default function JobsContainer({
+  searchFormValues,
+}: JobsContainerProps) {
+  const [jobsData, setJobsData] = useState(initialState);
+
   const {
-    state: { jobs, totalJobs, currentPage, isLoading },
+    state: { isLoading },
     getJobs,
   } = useAppContext();
 
+  async function fetchJobs() {
+    try {
+      const allJobsData = await getJobs(searchFormValues);
+      setJobsData(allJobsData);
+    } catch (error) {
+      setJobsData(initialState);
+    }
+  }
+
   useEffect(() => {
-    getJobs();
-  }, []);
+    fetchJobs();
+  }, [searchFormValues]);
 
   if (isLoading) {
     return <Loading center />;
   }
 
-  if (jobs.length === 0) {
+  if (jobsData.jobs.length === 0) {
     return (
       <Wrapper>
         <h2>No jobs to display...</h2>
@@ -29,10 +52,10 @@ export default function JobsContainer() {
   return (
     <Wrapper>
       <h5>
-        {totalJobs} job{totalJobs > 1 && 's'} found
+        {jobsData.totalJobs} job{jobsData.totalJobs > 1 && 's'} found
       </h5>
       <div className='jobs'>
-        {jobs.map((job) => {
+        {jobsData.jobs.map((job) => {
           return <Job key={job._id} job={job} />;
         })}
       </div>
