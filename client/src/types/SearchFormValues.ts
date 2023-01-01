c31f80a7d3c5c2fbb577d75ff9139601701f80ce
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-01 18:13:56 -0300

    feat(client): add SearchFormValues.ts file and implement its types

---
+++ client/src/types/SearchFormValues.ts
@@ -0,0 +1,15 @@
+import { JobSortOptions, JobStatusOptions, JobTypeOptions } from './Job';
+
+export enum AllOption {
+  all = 'all',
+}
+
+export const jobTypes = { ...AllOption, ...JobTypeOptions };
+export const jobStatus = { ...AllOption, ...JobStatusOptions };
+
+export type SearchFormValues = {
+  search: string;
+  status: keyof typeof jobStatus;
+  type: keyof typeof jobTypes;
+  sort: keyof typeof JobSortOptions;
+};
