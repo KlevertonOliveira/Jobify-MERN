ddd1f9274c594943640e1be404333e60e2a8bd6a
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-01 15:25:48 -0300

    feat(server): implements query params logic in getAllJobs controller
    Implements "search", "status", "type" and "sort" query parameters logic in getAllJobs controller.

--- src/controllers/jobs/getAllJobs.ts
+++ src/controllers/jobs/getAllJobs.ts
@@ -2,12 +2,54 @@ import Job from '@models/Job';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+interface QueryObject {
+  createdBy: string;
+  status?: string;
+  type?: string;
+  position?: Object;
+}
+
 export async function getAllJobs(req: Request, res: Response) {
-  const jobs = await Job.find({ createdBy: req.user.userId });  
-  
-  return res.status(StatusCodes.OK).json({ 
+  const { search, status, type, sort } = req.query;
+
+  const queryObject: QueryObject = {
+    createdBy: req.user.userId.valueOf(),
+  };
+
+  if (status && status !== 'all') {
+    queryObject.status = status as string;
+  }
+
+  if (type && type !== 'all') {
+    queryObject.type = type as string;
+  }
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' };
+  }
+
+  let sortOption: string;
+
+  switch (sort) {
+    case 'oldest':
+      sortOption = 'createdAt';
+      break;
+    case 'a-z':
+      sortOption = 'position';
+      break;
+    case 'z-a':
+      sortOption = '-position';
+      break;
+    default:
+      sortOption = '-createdAt';
+      break;
+  }
+
+  const jobs = await Job.find(queryObject).sort(sortOption);
+
+  return res.status(StatusCodes.OK).json({
     totalJobs: jobs.length,
     jobs,
     numberOfPages: 1,
-  })
+  });
 }
