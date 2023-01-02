7688cb2c1ce38c5ea87c8871db415ce2d754af62
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-02 15:20:35 -0300

    feat(server): add api rate limiter to authRoutes
    Limits the number of allowed api requests to Login and Register routes to a maximum of 10 every 15
    minutes based on IP with the package express-rate-limit.

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,8 +1,17 @@
 import { login, register, updateUser } from '@controllers/auth';
 import express from 'express';
 import { authenticateUser } from 'src/middleware/authenticateUser';
+import rateLimiter from 'express-rate-limit';
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message:
+    'Too many requests from this IP address. Please, try again after 15 minutes.',
+});
+
 export const authRouter = express.Router();
 
-authRouter.route('/login').post(login);
-authRouter.route('/register').post(register);
+authRouter.route('/register').post(apiLimiter, register);
+authRouter.route('/login').post(apiLimiter, login);
 authRouter.route('/updateUser').patch(authenticateUser, updateUser);
