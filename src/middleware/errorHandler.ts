4f59a311ced8bca34712493ab6ba270a76d440ac
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-02 15:15:28 -0300

    refactor(server): remove log message from errorHandler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,27 +3,33 @@ import { getReasonPhrase, StatusCodes } from 'http-status-codes';
 import { Error } from 'mongoose';
 import { MongoServerError } from 'mongoose/node_modules/mongodb';
 
-export function errorHandler(error, req: Request, res: Response, next: NextFunction) {
-  console.log(error);
-
+export function errorHandler(
+  error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const customError = {
     statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    message: error.message || getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
-  }
+    message:
+      error.message || getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+  };
 
   if (error instanceof Error.ValidationError) {
     customError.statusCode = StatusCodes.BAD_REQUEST;
-    customError.message = (
-      `Please, provide: ${Object.values(error.errors).map(field => field.message).join(', ')}`
-    )
+    customError.message = `Please, provide: ${Object.values(error.errors)
+      .map((field) => field.message)
+      .join(', ')}`;
   }
 
   if (error instanceof MongoServerError) {
     customError.statusCode = StatusCodes.BAD_REQUEST;
-    customError.message = `${Object.keys(error.keyValue).join(', ')} already in use`
+    customError.message = `${Object.keys(error.keyValue).join(
+      ', '
+    )} already in use`;
   }
 
   res.status(customError.statusCode).json({
-    message: customError.message
-  })
+    message: customError.message,
+  });
 }
