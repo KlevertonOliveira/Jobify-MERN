2aa09ab186eaba5c981e4f28f4b79915e9801d4b
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-04 15:34:53 -0300

    feat(server): implements checkTestUser middleware
    Creates a middleware to check if the logged user is Test User. This is necessary to provide a way of
    restricting access of this user only to specific routes.

---
+++ src/middleware/checkTestUser.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+import { BadRequestError } from 'src/errors';
+
+export function checkTestUser(req: Request, res: Response, next: NextFunction) {
+  if (req.user.userId === '63b43ac6ffc466d32b09e553') {
+    throw new BadRequestError(
+      'Test User is not authorized to perform this action!'
+    );
+  }
+  next();
+}
