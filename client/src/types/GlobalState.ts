2b28d5e7ad6890ad15da836bcb865ba20bf5af10
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 17:44:47 -0300

    feat(client): add isFetchingUserInfo property in GlobalState

--- client/src/types/GlobalState.ts
+++ client/src/types/GlobalState.ts
@@ -2,6 +2,7 @@ import { Alert } from './Alert';
 import { User } from './User';
 
 export interface GlobalState {
+  isFetchingUserInfo: boolean;
   isLoading: boolean;
   showAlert: boolean;
   showSidebar: boolean;
