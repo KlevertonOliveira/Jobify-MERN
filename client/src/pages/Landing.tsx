119400fb1b436fc2b13c36cc204cdb8d476983d1
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 17:52:06 -0300

    refactor(client): add redirect to dashboard page in Landing Pag  when user info is available
    In Landing Page, implements logic to immediately redirect user to dashboard page once user data is
    available.

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,27 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
+import { useAppContext } from '../contexts/appContext';
 
 export default function Landing() {
+  const {
+    state: { user },
+  } = useAppContext();
+
   return (
-    <Wrapper>
-      <nav>
-        <Logo />
-      </nav>
-      <div className='container page'>
-        {/* Info */}
-        <div className="info">
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eius repudiandae aliquam quae molestias obcaecati maxime consequatur iusto, animi quas delectus suscipit eum necessitatibus dignissimos ex voluptatum, distinctio nihil repellendus.</p>
-          <Link to={'/register'} className='btn btn-hero'>
-            Login/Register
-          </Link>
+    <>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* Info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+              eius repudiandae aliquam quae molestias obcaecati maxime
+              consequatur iusto, animi quas delectus suscipit eum necessitatibus
+              dignissimos ex voluptatum, distinctio nihil repellendus.
+            </p>
+            <Link to={'/register'} className='btn btn-hero'>
+              Login/Register
+            </Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
         </div>
-        <img src={main} alt="job hunt" className='img main-img' />
-      </div>
-    </Wrapper>
-  )
+      </Wrapper>
+    </>
+  );
 }
