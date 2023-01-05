7fd2a2ab3177127672f8bc5af9a5398f4d961fcb
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 18:27:09 -0300

    chore(server): add logout controller to controllers/auth/index.ts file

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,5 +1,6 @@
 import { getCurrentUser } from './getCurrentUser';
 import { login } from './login';
+import { logout } from './logout';
 import { register } from './register';
 import { updateUser } from './updateUser';
-export { login, register, updateUser, getCurrentUser };
+export { login, register, updateUser, getCurrentUser, logout };
