7710eaeaa96f3cd52a7abda8776c9e319791bfbf
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 16:09:19 -0300

    feat(server): create attachCookie function
    Implements attachCookie function. This function expects a response object and a token (string), then
    it attachs the token information to a cookie to be passed on inside the response object.

---
+++ src/utils/attachCookie.ts
@@ -0,0 +1,11 @@
+import { Response } from 'express';
+
+export function attachCookie(res: Response, token: string) {
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+}
