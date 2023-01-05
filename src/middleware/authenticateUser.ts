0ede82c6cd530bee8d3e28a2dfc16112c76ce07b
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 16:11:40 -0300

    refactor(server): refactor authenticateUser middleware
    Refactors authenticateUser middleware to retrieve token info from req.cookies instead of
    authentication header.

--- src/middleware/authenticateUser.ts
+++ src/middleware/authenticateUser.ts
@@ -2,21 +2,22 @@ import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { UnauthenticatedError } from 'src/errors';
 
-export function authenticateUser(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
+export function authenticateUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.cookies.token;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new UnauthenticatedError('Authentication Invalid')
+  if (!token) {
+    throw new UnauthenticatedError('Authentication Invalid');
   }
-
-  const token = authHeader.split(' ')[1];
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
     req.user = { userId: payload.userId };
     next();
   } catch (error) {
-    throw new UnauthenticatedError('Authentication Invalid')
+    throw new UnauthenticatedError('Authentication Invalid');
   }
-
 }
