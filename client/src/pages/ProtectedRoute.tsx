679cc6e4ac11ed8ebaa867703b0016419fe05358
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-01-05 17:48:42 -0300

    refactor(client): add Loading when fetching user info in ProtectedRoute

--- client/src/pages/ProtectedRoute.tsx
+++ client/src/pages/ProtectedRoute.tsx
@@ -1,11 +1,22 @@
 import { Navigate } from 'react-router-dom';
+import { Loading } from '../components';
 import { useAppContext } from '../contexts/appContext';
 
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const { state: { user } } = useAppContext();
+export default function ProtectedRoute({
+  children,
+}: {
+  children: JSX.Element;
+}) {
+  const {
+    state: { user, isFetchingUserInfo },
+  } = useAppContext();
+
+  if (isFetchingUserInfo) {
+    return <Loading center />;
+  }
 
   if (!user) {
-    return <Navigate to='/landing' />
+    return <Navigate to='/landing' />;
   }
 
   return children;
